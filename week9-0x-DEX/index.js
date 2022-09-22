7cbd07a3aa945130f268fb4682d3e94e0204cf6a
Russell Choudhury <russell.choudhury@gmail.com>
2022-09-22 18:21:16 +0100

    get price

--- week9-0x-DEX/index.js
+++ week9-0x-DEX/index.js
@@ -1,3 +1,5 @@
+const qs = require('qs');
+
 let currentTrade = {};
 let currentSelectSide;
 
@@ -72,6 +74,49 @@ async function closeModal() {
   document.getElementById("token_modal").style.display = "none";
 }
 
+async function getPrice() {
+  if (!currentTrade.from || !currentTrade.to || !document.getElementById("from_amount").value) return;
+
+  let amount = Number(document.getElementById("from_amount").value * 10 ** currentTrade.from.decimals);
+
+  const params = {
+    sellToken: currentTrade.from.address,
+    buyToken: currentTrade.to.address,
+    sellAmount: amount
+  }
+
+  // fetch the swap price
+  const response = await fetch(`https://api.0x.org/swap/v1/price?${qs.stringify(params)}`);
+  swapPriceJSON = await response.json();
+  console.log('Price', swapPriceJSON);
+
+  document.getElementById("to_amount").value = swapPriceJSON.buyAmount / (10 ** currentTrade.to.decimals);
+  document.getElementById("gas_estimate").innerHTML = swapPriceJSON.estimatedGas;
+}
+
+async function getQuote(address) {
+  if (!currentTrade.from || !currentTrade.to || !document.getElementById("from_amount").value) return;
+
+  let amount = Number(document.getElementById("from_amount").value * 10 ** currentTrade.from.decimals);
+
+  const params = {
+    sellToken: currentTrade.from.address,
+    buyToken: currentTrade.to.address,
+    sellAmount: amount,
+    takerAddress: address
+  }
+
+  // fetch the swap quote
+  const response = await fetch(`https://api.0x.org/swap/v1/quote?${qs.stringify(params)}`);
+  swapQuoteJSON = await response.json();
+  console.log('Quote', swapQuoteJSON);
+
+  document.getElementById("to_amount").value = swapQuoteJSON.buyAmount / (10 ** currentTrade.to.decimals);
+  document.getElementById("gas_estimate").innerHTML = swapQuoteJSON.estimatedGas;
+
+  return swapQuoteJSON;
+}
+
 init();
 document.getElementById("login_button").onclick = connect;
 document.getElementById("from_token_select").onclick = () => {
@@ -81,3 +126,4 @@ document.getElementById("to_token_select").onclick = () => {
   openModal("to");
 }
 document.getElementById("modal_close").onclick = closeModal;
+document.getElementById("from_amount").onblur = getPrice;
